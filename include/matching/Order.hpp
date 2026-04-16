c7daf9073746cdf3eb39d4d637640c68357d0791
tungminh111 <dmlocdmloc@gmail.com>
2026-04-17 02:45:49 +0800

    implement price level

--- include/matching/Order.hpp
+++ include/matching/Order.hpp
@@ -2,21 +2,11 @@
 
 #include <cstdint>
 
-enum OrderSide {
-    Buy,
-    Sell
-};
+enum OrderSide { Buy, Sell };
 
-enum OrderType {
-    Limit,
-    Market
-};
+enum OrderType { Limit, Market };
 
-enum OrderAction {
-    Create,
-    Modify,
-    Cancel
-};
+enum OrderAction { Create, Modify, Cancel };
 
 struct alignas(64) Order {
     int8_t order_id_;
@@ -26,5 +16,13 @@ struct alignas(64) Order {
     int8_t instrument_id_;
     OrderSide side_;
     OrderType type_;
-    OrderAction action_; 
+    OrderAction action_;
+};
+
+struct OrderQuantity {
+    OrderQuantity(int8_t order_id, int8_t quantity, bool match_full)
+        : order_id_(order_id), quantity_(quantity), match_full_(match_full) {}
+    int8_t order_id_;
+    int8_t quantity_;
+    bool match_full_;
 };
